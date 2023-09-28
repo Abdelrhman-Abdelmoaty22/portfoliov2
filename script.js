eeaa6d94146e5fe8f32d49b7249289af699157e4
Abdelrhman-Abdelmoaty22 <141934349+Abdelrhman-Abdelmoaty22@users.noreply.github.com>
2023-09-28 10:09:28 +0300

    portfolio version 2

--- script.js
+++ script.js
@@ -27,3 +27,17 @@ icon.onclick = () =>{
         icon.src = "sun.png";
     }
 };
+
+function sendToWhatsapp(){
+    let number = +201288415321;
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    var url = "https://wa.me/" + number + "?text="
+    + "Name : " + name + "%0a" + "E-mail : " + email
+    + "%0a" + "Message : " + message + "%0a%0a";
+    
+    window.open(url,'_blank').focus();
+}
